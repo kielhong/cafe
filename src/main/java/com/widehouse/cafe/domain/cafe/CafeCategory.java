bd42ffc3588d2fb325eefd1565c17dc4732b7255
kielhong <kielhong@naver.com>
2017-02-11 16:21:03 -0500

    CafeCategory 추가

---
+++ src/main/java/com/widehouse/cafe/domain/cafe/CafeCategory.java
@@ -0,0 +1,15 @@
+package com.widehouse.cafe.domain.cafe;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by kiel on 2017. 2. 11..
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class CafeCategory {
+    private String name;
+}
