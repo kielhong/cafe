46f71cf9804f75b33d9b4e115f17562e8b2ce43f
kielhong <kielhong@naver.com>
2017-02-12 12:47:51 -0500

    #1 create Comment

--- src/main/java/com/widehouse/cafe/domain/board/Comment.java
+++ src/main/java/com/widehouse/cafe/domain/board/Comment.java
@@ -1,7 +1,27 @@
 package com.widehouse.cafe.domain.board;
 
+import com.widehouse.cafe.domain.member.Member;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
 /**
  * Created by kiel on 2017. 2. 11..
  */
+@Getter
 public class Comment {
+    private Article article;
+
+    private Member commenter;
+
+    private String comment;
+
+    private LocalDateTime createDateTime;
+
+    public Comment(Article article, Member commenter, String comment) {
+        this.article = article;
+        this.commenter = commenter;
+        this.comment = comment;
+        this.createDateTime = LocalDateTime.now();
+    }
 }
