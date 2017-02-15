aec260fb1184387571b90d9ec5940240d24b6b8a
kielhong <kielhong@naver.com>
2017-02-15 18:12:28 -0500

    CafeStatistics.decreaseCommentCount() 최소값은 0

---
+++ src/test/java/com/widehouse/cafe/domain/cafe/CafeStatisticsTest.java
@@ -0,0 +1,22 @@
+package com.widehouse.cafe.domain.cafe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+/**
+ * Created by kiel on 2017. 2. 15..
+ */
+public class CafeStatisticsTest {
+    @Test
+    public void decreaseCommentCount_should_set_commentCount_minimum_zero() {
+        // given
+        CafeStatistics cafeStatistics = new CafeStatistics();
+        cafeStatistics.setCafeCommentCount(-1L);
+        // when
+        cafeStatistics.decreaseCommentCount();
+        // then
+        assertThat(cafeStatistics.getCafeCommentCount())
+                .isEqualTo(0);
+    }
+}
