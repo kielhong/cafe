fceb8da0abb0d73c4b40547edafd02c832f2ce54
kielhong <kielhong@naver.com>
2017-02-19 19:16:18 -0500

    #13 ArticleService.getArticlesByBoard()

--- src/main/java/com/widehouse/cafe/service/ArticleService.java
+++ src/main/java/com/widehouse/cafe/service/ArticleService.java
@@ -4,6 +4,7 @@ import static org.springframework.data.domain.Sort.Direction.DESC;
 
 import com.widehouse.cafe.domain.article.Article;
 import com.widehouse.cafe.domain.article.ArticleRepository;
+import com.widehouse.cafe.domain.cafe.Board;
 import com.widehouse.cafe.domain.cafe.Cafe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -18,10 +19,17 @@ import java.util.List;
 @Service
 public class ArticleService {
     @Autowired
-    ArticleRepository articleRepository;
+    private ArticleRepository articleRepository;
 
     public List<Article> getArticlesByCafe(Cafe cafe, int page, int size) {
         List<Article> articles = articleRepository.findByCafe(cafe,
+                new PageRequest(page, size, new Sort(DESC, "id")));
+
+        return articles;
+    }
+
+    public List<Article> getArticlesByBoard(Board board, int page, int size) {
+        List<Article> articles = articleRepository.findByBoard(board,
                 new PageRequest(page, size, new Sort(DESC, "id")));
 
         return articles;
--- src/test/java/com/widehouse/cafe/service/ArticleServiceTest.java
+++ src/test/java/com/widehouse/cafe/service/ArticleServiceTest.java
@@ -34,22 +34,31 @@ public class ArticleServiceTest {
     private ArticleService articleService;
 
     private Cafe cafe;
-    private Board board;
+    private Board board1;
+    private Board board2;
     private Member writer;
+
+    private Article article1;
+    private Article article2;
+    private Article article3;
+    private Article article4;
 
     @Before
     public void setUp() {
         cafe = new Cafe("testurl", "testcafe");
-        board = new Board(cafe, "board");
+        board1 = new Board(cafe, "board1");
+        board2 = new Board(cafe, "board2");
         writer = new Member("writer");
+
+        Article article1 = new Article(board1, writer, "test article1", "test1");
+        Article article2 = new Article(board1, writer, "test article2", "test2");
+        Article article3 = new Article(board2, writer, "test article3", "test3");
+        Article article4 = new Article(board2, writer, "test article3", "test3");
     }
 
     @Test
-    public void getArticlesByCafe_Should_Return_ListArticleWithIdOrderDesc() {
+    public void getArticlesByCafe_Should_Return_ListArticleInCafeWithIdOrderDesc() {
         // given
-        Article article1 = new Article(board, writer, "test article1", "test1");
-        Article article2 = new Article(board, writer, "test article2", "test2");
-        Article article3 = new Article(board, writer, "test article3", "test3");
         given(articleRepository.findByCafe(cafe, new PageRequest(0, 3, new Sort(DESC, "id"))))
                 .willReturn(Arrays.asList(article3, article2, article1));
         // when
@@ -57,6 +66,17 @@ public class ArticleServiceTest {
         // then
         then(articles)
                 .containsExactly(article3, article2, article1);
+    }
 
+    @Test
+    public void getArticlesByBoard_Should_Return_ListArticleInBoardWithIdOrderDesc() {
+        // given
+        given(articleRepository.findByBoard(board1, new PageRequest(0, 3, new Sort(DESC, "id"))))
+                .willReturn(Arrays.asList(article2, article1));
+        // when
+        List<Article> articles = articleService.getArticlesByBoard(board1, 0, 3);
+        // then
+        then(articles)
+                .containsExactly(article2, article1);
     }
 }
