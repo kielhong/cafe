4afddc489947049155db17900dd74bff34e08fd8
kielhong <kielhong@naver.com>
2017-02-20 00:59:58 -0500

    #15 Json date time format

---
+++ src/test/java/com/widehouse/cafe/web/JsonFormatTest.java
@@ -0,0 +1,33 @@
+package com.widehouse.cafe.web;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.widehouse.cafe.domain.cafe.Category;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by kiel on 2017. 2. 20..
+ */
+@RunWith(SpringRunner.class)
+@JsonTest
+public class JsonFormatTest {
+    @Autowired
+    private JacksonTester<Category> json;
+
+    @Test
+    public void testSerialize() throws Exception {
+        LocalDateTime datetime = LocalDateTime.of(2017, 2, 15, 13, 14, 15);
+        Category category = new Category(1L, "test", datetime, 1);
+
+        assertThat(this.json.write(category)).hasJsonPathStringValue("@.create_date_time");
+        assertThat(this.json.write(category)).extractingJsonPathStringValue("@.create_date_time")
+                .isEqualTo("2017-02-15T13:14:15");
+    }
+}
