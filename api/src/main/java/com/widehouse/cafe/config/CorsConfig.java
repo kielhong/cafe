1eeb4d84af3e4cacee9fb7d5d745b16dc95618ad
kielhong <kielhong@naver.com>
2017-02-23 14:31:52 -0500

    Api CORS 처리

---
+++ api/src/main/java/com/widehouse/cafe/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.widehouse.cafe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by kiel on 2017. 2. 22..
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/categories").allowedOrigins("http://localhost:8081");
+            }
+        };
+    }
+}
