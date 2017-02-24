15c66bda1c55b9613621f92e520d6a69b47d01a7
kielhong <kielhong@naver.com>
2017-02-24 15:46:46 -0500

    section view  정

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 /** Cafe Category **/
 INSERT INTO category (name, create_date_time, list_order) VALUES ('게임',now(),1),('만화/애니메이션',now(),2),('방송/연예',now(),3),('문화/예술',now(),4),('영화',now(),5),('음악',now(),6),('팬카페',now(),7),('여행',now(),8),('스포츠/레저',now(),9),('애완동물',now(),10),('취미',now(),11),('생활',now(),12);
 
+INSERT INTO cafe(category_id, url, name, description, visibility, cafe_article_count, cafe_comment_count, cafe_member_count, cafe_visit_count, create_date_time) values (1,'cafetest','테스트 카페','테스트용 카페입니다','PUBLIC', 12345, 1234566, 160540, 1233, '2003-12-21 01:02:03');
 INSERT INTO cafe(category_id, url, name, description, visibility, cafe_article_count, cafe_comment_count, cafe_member_count, cafe_visit_count, create_date_time) values (1,'gamecafe1','서머너즈워:공식카페','서머너즈워 공식카페입니다','PUBLIC', 843143, 1231312, 160540, 123123, '2014-03-21 01:02:03');
 INSERT INTO cafe(category_id, url, name, description, visibility, cafe_article_count, cafe_comment_count, cafe_member_count, cafe_visit_count, create_date_time) values (1,'gamecafe2','큐라레: 마법도서관 공식 카페','큐라레 공식카페입니다','PUBLIC', 898790, 1231312, 58582, 123123, '2014-03-21 01:02:03');
 INSERT INTO cafe(category_id, url, name, description, visibility, cafe_article_count, cafe_comment_count, cafe_member_count, cafe_visit_count, create_date_time) values (1,'gamecafe3','샌드박스 팬 카페','샌드박스 팬 카페입니다','PUBLIC', 898790, 1231312, 58582, 123123, '2014-03-21 01:02:03');
