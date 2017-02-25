73dabb64dc1c495758454978c3171bb286de644b
kielhong <kielhong@naver.com>
2017-02-25 16:03:15 -0500

    #20 Cors 처리를 global 하게 추가

--- api/src/main/java/com/widehouse/cafe/web/CategoryController.java
+++ api/src/main/java/com/widehouse/cafe/web/CategoryController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +35,6 @@ public class CategoryController {
         return categories;
     }
 
-    @CrossOrigin(origins = "http://localhost:8081")
     @GetMapping("/categories/{categoryId}/cafes")
     public List<CafeSummary> getCafesByCategory(@PathVariable Long categoryId,
                                                 @PageableDefault(page = 0, size = 10,
