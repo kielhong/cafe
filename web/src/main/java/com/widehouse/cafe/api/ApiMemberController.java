bc67fbd6f290ccc4b39d3926cc34d0e5cca454d0
kielhong <kielhong@naver.com>
2017-03-04 23:31:30 -0500

    특정 member 대신 current login user 에 대해서만 값을 반환하도록 수정

--- web/src/main/java/com/widehouse/cafe/api/ApiMemberController.java
+++ web/src/main/java/com/widehouse/cafe/api/ApiMemberController.java
@@ -2,14 +2,14 @@ package com.widehouse.cafe.api;
 
 import com.widehouse.cafe.domain.cafe.Cafe;
 import com.widehouse.cafe.domain.member.Member;
-import com.widehouse.cafe.domain.member.MemberRepository;
+import com.widehouse.cafe.service.MemberDetailsService;
 import com.widehouse.cafe.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,17 +18,17 @@ import java.util.List;
  * Created by kiel on 2017. 2. 15..
  */
 @RestController
+@RequestMapping("api")
 public class ApiMemberController {
     @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
     private MemberService memberService;
+    @Autowired
+    private MemberDetailsService memberDetailsService;
 
-    @GetMapping("/members/{username}/cafes")
-    public List<Cafe> getCafesByMember(@PathVariable String username,
-                                       @PageableDefault(sort = "cafe.createDateTime",
+    @GetMapping("/members/my/cafes")
+    public List<Cafe> getCafesByMember(@PageableDefault(sort = "cafe.createDateTime",
                                                direction = Sort.Direction.DESC) Pageable pageable) {
-        Member member = memberRepository.findByUsername(username);
+        Member member = memberDetailsService.getCurrentMember();
 
         return memberService.getCafesByMember(member, pageable);
     }
--- web/src/test/java/com/widehouse/cafe/api/ApiMemberControllerTest.java
+++ web/src/test/java/com/widehouse/cafe/api/ApiMemberControllerTest.java
@@ -8,7 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.widehouse.cafe.domain.cafe.Cafe;
 import com.widehouse.cafe.domain.member.Member;
-import com.widehouse.cafe.domain.member.MemberRepository;
+import com.widehouse.cafe.service.MemberDetailsService;
 import com.widehouse.cafe.service.MemberService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,21 +35,20 @@ public class ApiMemberControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private MemberRepository memberRepository;
+    private MemberDetailsService memberDetailsService;
     @MockBean
     private MemberService memberService;
 
     @Test
     public void getCafesByMember() throws Exception {
-        String username = "tester";
-        Member member = new Member(username);
-        given(this.memberRepository.findByUsername(username))
+        Member member = new Member("tester");
+        given(this.memberDetailsService.getCurrentMember())
                 .willReturn(member);
         given(this.memberService.getCafesByMember(member, new PageRequest(0, 10, new Sort(Sort.Direction.DESC, "cafe.createDateTime"))))
                 .willReturn(Arrays.asList(new Cafe("url1", "name1"), new Cafe("url2", "name2"),
                         new Cafe("url3", "name3"), new Cafe("url4", "name4")));
         // then
-        this.mockMvc.perform(get("/members/" + username + "/cafes"))
+        this.mockMvc.perform(get("/api/members/my/cafes"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$.length()").value(4))
@@ -57,24 +56,19 @@ public class ApiMemberControllerTest {
                 .andExpect(jsonPath("$.[1].url").value("url2"))
                 .andExpect(jsonPath("$.[2].url").value("url3"))
                 .andExpect(jsonPath("$.[3].url").value("url4"));
-        this.mockMvc.perform(get("/members/" + 2L + "/cafes"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.length()").value(0));
     }
 
     @Test
     public void getCafesByMemberWithPaging() throws Exception {
-        String username = "tester";
-        Member member = new Member(username);
-        given(this.memberRepository.findByUsername(username))
+        Member member = new Member("tester");
+        given(this.memberDetailsService.getCurrentMember())
                 .willReturn(member);
         given(this.memberService.getCafesByMember(member,
                 new PageRequest(0, 3, new Sort(Sort.Direction.DESC, "cafe.createDateTime"))))
                 .willReturn(Arrays.asList(new Cafe("url1", "name1"), new Cafe("url2", "name2"),
                         new Cafe("url3", "name3")));
         // then
-        this.mockMvc.perform(get("/members/" + username + "/cafes?page=0&size=3&sort=cafe.createDateTime,desc"))
+        this.mockMvc.perform(get("/api/members/my/cafes?page=0&size=3&sort=cafe.createDateTime,desc"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$.length()").value(3))
